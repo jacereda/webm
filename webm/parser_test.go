27ff6e10b41453dd94085e7d8e3d092357c4da5d
Jorge Acereda <jacereda@gmail.com>
2012-09-13 00:09:54 +0200

    Added webm sources

---
+++ webm/parser_test.go
@@ -0,0 +1,20 @@
+package webm
+
+import (
+	"bufio"
+	"ebml/ebml"
+	"testing"
+	"os"
+)
+
+func TestReadStruct(t *testing.T) {
+	path := "/Users/jacereda/Downloads/big-buck-bunny_trailer.webm"
+	r,err := os.Open(path)
+	if err != nil {
+		t.Fatal("unable to open file " + path)
+	}
+	br := bufio.NewReader(r)
+	var w WebM
+	err = ebml.Read(br, &w)
+	t.Logf("%v\n%+v\n", err, w)
+}
